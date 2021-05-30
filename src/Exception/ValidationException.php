bffef269959fcfb8512fb6db97356152601c97a7
Colin O'Dell <colinodell@gmail.com>
2021-05-30 15:59:50 -0400

    Fix copyright headers

--- src/Exception/ValidationException.php
+++ src/Exception/ValidationException.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the league/commonmark package.
+ * This file is part of the league/config package.
  *
  * (c) Colin O'Dell <colinodell@gmail.com>
  *
